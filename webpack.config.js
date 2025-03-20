3add00369ca9f83ec55baf8a26ecc782954adde8
DeAsia7 <asiamallory277@gmail.com>
2025-03-20 00:45:32 -0400

    updste

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,50 @@
-
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
+entry: './src/index.js',
+output: {
+filename: 'bundle.js',
+path: path.resolve(__dirname,
+'dist'),
+clean: true,
+},
+module: {
+rules: [
+{ test: /\.css$/, use: ['style-loader',"css-loader"]},
+{ test: /\.(mp3|wav)$/, use: ['file-loader'] },
+{
+    test: /\.js$/,
+    exclude: /mode_modules/,
+    use: {
+        loader: "babel-loader",
+    },
+},
+{
+
+    test: /\.(scss|css)$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "postcss-loader",
+        "sass-loader",
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
-  devServer: {
-    static: "./dist",
-    hot: true,
-  },
-};
+}
+],
 
 
+},
+plugins: [
+new HtmlWebpackPlugin({
+template: './src/index.html',
+}),
+new MiniCssExtractPlugin(),
+],
+devServer: {
+static: './dist',
+port: 8080,
+hot: true,
+open: true,
+},
+mode: 'development', // or 'production' include minimization
+}
